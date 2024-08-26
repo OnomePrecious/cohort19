9f4b6bd934b11eebd3a0edf8bc85a9d2c14b2e3d
Precious <preciousonome002@gmail.com>
2024-08-26 18:48:49 +0100

    bug fixed in register

--- src/main/java/org/example/SmallJavaApp.java
+++ src/main/java/org/example/SmallJavaApp.java
@@ -7,7 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmallJavaApp {
     public static void main(String[] args) {
         SpringApplication.run(SmallJavaApp.class, args);
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
     }
 }
